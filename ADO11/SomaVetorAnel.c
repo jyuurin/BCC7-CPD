299998da10b46ec8ef5db932d9ebe649fa45a5f8
Julia <84683597+jyuurin@users.noreply.github.com>
2024-05-25 02:15:03 -0300

    Create SomaVetorAnel.c

---
+++ ADO11/SomaVetorAnel.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+	int rank, tamanho;
+	int n = 12; // Tamanho do vetor total
+	int r; // Tamanho do sub-vetor
+	int *localVet;
+	int localSoma = 0;
+	int totalSoma = 0;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &tamanho);
+
+	r = n / tamanho;
+	localVet = (int *)malloc(r * sizeof(int));
+
+	for (int i = 0; i < r; i++) {
+		localVet[i] = (rank * r) + i + 1;
+		localSoma += localVet[i];
+	}
+
+	// Realiza a comunicação no anel
+	int esq = (rank == 0) ? (tamanho - 1) : (rank - 1);
+	int dir = (rank + 1) % tamanho;
+
+	if (rank == 0) {
+		MPI_Send(&localSoma, 1, MPI_INT, dir, 0, MPI_COMM_WORLD);
+		MPI_Recv(&totalSoma, 1, MPI_INT, esq, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	} else {
+		MPI_Recv(&totalSoma, 1, MPI_INT, esq, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		localSoma += totalSoma;
+		MPI_Send(&localSoma, 1, MPI_INT, dir, 0, MPI_COMM_WORLD);
+	}
+
+	if (rank == 0) 
+		printf("Total: %d\n", totalSoma);
+
+	free(localVet);
+	return 0;
+}
